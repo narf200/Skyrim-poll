aa8d2674c0af4b937d715fd51b42875f8dc820d8
narf200 <narf200@gmail.com>
2023-03-01 18:34:04 +0100

    feature/SP-007_add_result_modal

---
+++ src/pages/PollPage/components/Modal.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import getCalculationCharacter from '../helpers/helpers';
+import styles from './Modal.module.less';
+
+export default function Modal({ answerCounter, handleResetCounter }) {
+  const { name, url } = getCalculationCharacter(answerCounter);
+
+  return (
+    <div className={styles.modal_backdrop}>
+      <div className={styles.modal}>
+        <h1 className={styles.heading_primary}>You are a {name}</h1>
+        <img className={styles.character_image} src={url} alt={name} />
+        <button onClick={handleResetCounter} className={styles.btn_result}>
+          <Link to="/">Take the test again</Link>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+Modal.propTypes = {
+  handleResetCounter: PropTypes.func,
+  answerCounter: PropTypes.number,
+};
+
+Modal.defaultProps = {
+  handleResetCounter: PropTypes.func,
+  answerCounter: PropTypes.number,
+};
--- src/pages/PollPage/helpers/helpers.js
+++ src/pages/PollPage/helpers/helpers.js
@@ -1,15 +1,25 @@
-import { ASSASIN, CATFUNNY, SELLER, WIZARD } from '../constants/constants';
-
 const getCalculationCharacter = (counter) => {
-  let character = '';
+  let character = {};
   if (counter >= 6) {
-    character = ASSASIN;
+    character = {
+      name: 'Assasin',
+      url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.g7Q0tmp4NlhlbdoWj5ebPwHaHa%26pid%3DApi&f=1&ipt=8b16ab764ab9ae5844a1ef88633ec1463f208d62b3068fc0e9ba682c29a2c36c&ipo=images',
+    };
   } else if (counter >= 1 && counter <= 6) {
-    character = WIZARD;
+    character = {
+      name: 'Wizard',
+      url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.RZMGGl4Yhpgvz44jUixJlgHaKz%26pid%3DApi&f=1&ipt=cedcdc988eea528d0ac2738d53209062fe5599650d51acba650b2f36123db121&ipo=images',
+    };
   } else if (counter <= -5 && counter >= -10) {
-    character = SELLER;
-  } else if (counter <= -11) {
-    character = CATFUNNY;
+    character = {
+      name: 'Seller',
+      url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.7beE9LEaSBEoRBiTE5tOsAHaKe%26pid%3DApi&f=1&ipt=697fc0e23dce42940b4068db3969b77b4b3b214b27392052890d3ed54204a211&ipo=images',
+    };
+  } else if (counter <= -11 && counter === 0) {
+    character = {
+      name: 'Funny cat',
+      url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.tE02XcxrmA9P2_uKBIc4DgHaJ4%26pid%3DApi&f=1&ipt=bb87ceb201f764fd8de594c534d7d4a3481de9bacbcd9c92819a4a3e02e39713&ipo=images',
+    };
   }
   return character;
 };
