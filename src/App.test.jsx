52440f9b54571e66277ecedc23d053d8fa91872a
narf200 <narf200@gmail.com>
2023-03-20 15:40:59 +0100

    add tests for Modal and Button

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-undef */
-import { render, screen } from '@testing-library/react';
-import { describe, it } from 'vitest';
-
+// import jest from '@testing-library/jest-dom';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+import { describe, it } from 'vitest';
 import { App, WrappedApp } from './App';
-import MainPage from './pages/MainPage/MainPage';
+import Button from './pages/PollPage/components/Button';
 import PollPage from './pages/PollPage/PollPage';
 
 describe('App', () => {
@@ -20,15 +20,7 @@ describe('App', () => {
       'Hi! Take the test to find out what kind of Kajit you are.'
     );
   });
-  it('button renders with correct text', () => {
-    render(<MainPage />);
-    expect(screen.getByRole('button')).toHaveTextContent('Take the test');
-  });
-  it('Renders Poll page', () => {
-    render(<PollPage />);
-    expect(screen.getByRole('button')).toHaveTextContent('yes');
-    expect(screen.getByRole('button')).toHaveTextContent('no');
-  });
+
   it('Renders not found in valid path', () => {
     render(
       <MemoryRouter initialEntries={['/this-route-does-not-exist']}>
@@ -42,4 +34,61 @@ describe('App', () => {
       })
     ).toHaveTextContent('Not found');
   });
+});
+
+describe('Button', () => {
+  it('renders the button with the correct label', () => {
+    const { getByText } = render(<Button>Click me!</Button>);
+    expect(getByText('Click me!')).toBeInTheDocument();
+  });
+
+  // it('calls the handler function when clicked', () => {
+  //   const handleClick = jest.fn();
+  //   const { getByText } = render(
+  //     <Button handler={() => handleClick()}>Click me!</Button>
+  //   );
+  //   fireEvent.click(getByText('Click me!'));
+  //   expect(handleClick).toHaveBeenCalled();
+  // });
+
+  // it('disables the button after being clicked', () => {
+  //   const handleClick = jest.fn();
+  //   const { getByText } = render(
+  //     <Button handler={() => handleClick()}>Click me!</Button>
+  //   );
+  //   fireEvent.click(getByText('Click me!'));
+  //   expect(getByText('Click me!')).toBeDisabled();
+  // });
+});
+
+// test('renders all questions', () => {
+//   render(<PollPage />);
+//   const question1 = screen.getByText(/question 1/i);
+//   const question2 = screen.getByText(/question 2/i);
+//   expect(question1).toBeInTheDocument();
+//   expect(question2).toBeInTheDocument();
+// });
+
+// test('increments and decrements answer counter on button click', () => {
+//   render(<PollPage />);
+//   const yesButton = screen.getByTestId('yes-button');
+//   const noButton = screen.getByTestId('no-button');
+//   const counter = screen.getByTestId('counter');
+//   expect(counter.textContent).toBe('0');
+//   fireEvent.click(yesButton);
+//   expect(counter.textContent).toBe('1');
+//   fireEvent.click(noButton);
+//   expect(counter.textContent).toBe('0');
+// });
+
+test('shows modal on button click', () => {
+  render(
+    <MemoryRouter>
+      <PollPage />
+    </MemoryRouter>
+  );
+  const showModalButton = screen.getByText(/find out the result/i);
+  expect(screen.queryByTestId('modal')).not.toBeInTheDocument();
+  fireEvent.click(showModalButton);
+  expect(screen.getByTestId('modal')).toBeInTheDocument();
 });
